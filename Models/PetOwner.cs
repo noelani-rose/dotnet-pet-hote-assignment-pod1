8ec7538aaf99e56a943a3db976584005eeb0d314
Tormod Sletteboe <tormod.slettebo@gmail.com>
2022-12-01 13:55:27 -0600

    petowners

--- Models/PetOwner.cs
+++ Models/PetOwner.cs
@@ -5,5 +5,18 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 namespace pet_hotel
 {
-    public class PetOwner {}
+    public class PetOwner
+    {
+        public int id { get; set; }
+
+        [Required]
+        public string emailAddress { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string name { get; set; }
+
+        [NotMapped]
+        public int petCount { get; set;}
+    }
 }
